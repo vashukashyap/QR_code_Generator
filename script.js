44c2d4257fd6b92a28c732767d4f8872fe919a48
vashu kashyap <kashyapvashu2003@gmail.com>
2023-11-08 20:08:15 +0530

    added download qr

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&format=png&data=";
 
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
+let hrefImage = document.getElementById("imageLink");
 let qrText = document.getElementById("qrText");
 
 const generateQR = () => {
   if (qrText.value.length > 0) {
-    qrImage.src = URL + qrText.value;
+    let qr = URL + qrText.value;
+    qrImage.src = qr;
     imgBox.classList.add("show-img");
+    hrefImage.addEventListener("click", async () => {
+      let qrCode = await fetch(qr);
+      let blob = await qrCode.blob();
+      console.log(blob);
+      hrefImage.href = window.URL.createObjectURL(blob);
+      hrefImage.download = qrText.value + '.jpg';
+    });
   } else {
     qrText.classList.add("error");
     setTimeout(() => {
